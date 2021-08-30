717e4db6b45fdfbc2dd0579e2d5430bc13ed0585
Jaime Zamora <kratov@protonmail.com>
2021-08-29 20:28:45 -0500

    Post query-service

--- app/src/Components/CommentList.js
+++ app/src/Components/CommentList.js
@@ -1,15 +1,7 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 
-export const CommentList = ({id}) => {
-    const [comments, setComments] = useState([]);
-    const fetchComments = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${id}/comments`);
-        setComments(res.data);
-    }
-    useEffect(() => {
-        fetchComments();
-    },[])
+export const CommentList = ({comments}) => {
     const renderComments =  comments.map(comment => {
         return <li key={comment.id}>
             {comment.content}
--- app/src/Components/PostList.js
+++ app/src/Components/PostList.js
@@ -6,8 +6,9 @@ import {CommentList} from "./CommentList";
 export const PostList = () => {
     const [posts, setPosts] = useState({});
     const fetchPost = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
+        const res = await axios.get('http://localhost:4002/posts');
         setPosts(res.data);
+        console.log(res.data)
     }
     useEffect(() => {
         fetchPost();
@@ -16,7 +17,7 @@ export const PostList = () => {
         return <div className={'card'} style={{width: '30%', marginBottom: '20px'}} key={post.id}>
             <div className={"card-body"}>
                 <h3>{post.title}</h3>
-                <CommentList id={post.id}/>
+                <CommentList comments={post.comments}/>
                 <CommentCreate id={post.id}/>
             </div>
         </div>
